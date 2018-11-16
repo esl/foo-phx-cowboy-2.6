eecec8af818b50616b9a034e9bc1d63269f495ff
bryan <bryan.hunt@erlang-solutions.com>
2018-11-16 16:08:56 +0000

    tidy mix configs remove prod.secret loading

--- config/config.exs
+++ config/config.exs
@@ -1,8 +1,3 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
 use Mix.Config
 
 # General application configuration
@@ -21,6 +16,5 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:user_id]
 
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
+# Import environment specific config. Can override anything previously defined.
 import_config "#{Mix.env()}.exs"
