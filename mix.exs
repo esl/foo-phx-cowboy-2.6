295c4f5ce05937be77eeadba1009960ba1aab2dd
bryan <bryan.hunt@erlang-solutions.com>
2018-11-19 16:46:36 +0000

    mix.exs (tidy + add rexbug)

--- mix.exs
+++ mix.exs
@@ -34,14 +34,6 @@ defmodule Foo.Mixfile do
     end
   end
 
-  # Type `mix help compile.app` for more information.
-  #   def application do
-  #       [mod: {PhoenixDistillery, []},
-  #            applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-  #                                :phoenix_ecto, :postgrex]]
-  #                                end
-  #
-
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
@@ -56,10 +48,10 @@ defmodule Foo.Mixfile do
       {:phoenix, "~> 1.4.0"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
-      # {:ecto, "~> 2.2"},
       {:postgrex, ">= 0.13.5"},
       {:poison, "~> 2.2"},
       {:phoenix_html, "~> 2.10"},
+      {:rexbug, ">= 1.0.0", only: :dev},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:plug_cowboy, "~> 2.0.0"},
